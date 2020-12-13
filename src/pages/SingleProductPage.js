76f8980e40a8b6364a183456d2cce29288422a1e
Dat <haruthong@gmail.com>
2020-12-13 13:21:19 -0800

    render singleProduct Image, display it by user "Click"

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -34,7 +34,45 @@ const SingleProductPage = () => {
   if (loading) return <Loading />
   if (error) return <Error />
 
-  return <h4>single product page</h4>
+  const { name, price, description, stock, stars, reviews, id:sku, company, images } = product
+
+  return (
+		<Wrapper>
+      <PageHero title={name} product={product} />
+      
+			<div className="section section-center page">
+				<Link to="./products" className="btn">
+          Back to Products
+				</Link>
+        
+				<div className="product-center">
+          <ProductImages images={images}/>
+          
+					<section className="content">
+						<h2>{name}</h2>
+            <Stars />
+						<h5 className="price">{formatPrice(price)}</h5>
+						<p className="desc">{description}</p>
+						<p className="info">
+							<span>Available:</span>
+							{stock > 0 ? "In Stock" : "out of stock"}
+						</p>
+						<p className="info">
+							<span>SKU:</span>
+							{sku}
+						</p>
+						<p className="info">
+							<span>Brand:</span>
+              {company}
+            </p>
+            <hr />
+
+            {stock > 0 && <AddToCart />}
+					</section>
+				</div>
+			</div>
+		</Wrapper>
+	);
 }
 
 const Wrapper = styled.main`
