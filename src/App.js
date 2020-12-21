ecc5cc4b146637a44495a415b8c6eac10a95cf07
Dat <haruthong@gmail.com>
2020-12-20 23:34:42 -0800

    Revert "project completed"
    This reverts commit 53e080e25403042574eaab90e324db251252b28e.

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
 					<Products />
 				</Route>
         <Route exact path="/products/:id" children={<SingleProduct />} />
-        <PrivateRoute exact path="/checkout">
+        <Route exact path="/checkout">
           <Checkout />
-        </PrivateRoute>
+        </Route>
         <Route path="*">
           <Error />
         </Route>
